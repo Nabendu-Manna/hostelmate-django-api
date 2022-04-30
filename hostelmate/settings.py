ecb832726b94fcfdc709614b7cfdcc0b1671b286
Nabendu Manna <69715068+Nabendu-Manna@users.noreply.github.com>
2022-04-30 11:29:02 +0530

    1st

--- hostelmate/settings.py
+++ hostelmate/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 'rest_framework_simplejwt'
+
     'accounts'
 ]
 
@@ -50,6 +52,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 ROOT_URLCONF = 'hostelmate.urls'
 
--- hostels/views.py
+++ hostels/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
 
-from rest_framework.views import APIView
-
 # Create your views here.
