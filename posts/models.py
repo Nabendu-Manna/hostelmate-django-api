13adb5416596a8fa00f90aa496e454df560b7653
Nabendu Manna <69715068+Nabendu-Manna@users.noreply.github.com>
2022-05-08 23:07:52 +0530

    post model

---
+++ posts/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+from accounts.models import LandlordProfile
+from hostels.models import Room
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator, MinLengthValidator
+
+from hostelmate import settings
+
+# class RoomImage(models.Model):
+#     pass
+
+class RoomPost(models.Model):
+    room = models.ForeignKey(Room, verbose_name="Room", on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, default=None, blank=True, null=True)
+    body = models.CharField(max_length=300, default=None, blank=True, null=True)
+
+    @property
+    def images(self):
+        images = self.roomimage_set.all()
+        return images
+    
+    def __str__(self):
+        return self.title + " - " + self.room.name
+
+
+class RoomPostImage(models.Model):
+    post = models.ForeignKey(Room, verbose_name="Room", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/posts/')
+
+    def __str__(self):
+        return self.pk + " - " + self.post.title + " - " + self.image
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
